86bfd7983feede40fb801dc20c6c8049732d3c44
Robert Dominguez <dominguez.1st@gmail.com>
2020-10-05 17:03:24 -0600

    Mine seems to be working.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/MainActivity.java
@@ -3,8 +3,6 @@ package edu.cnm.deepdive.codebreaker.controller;
 import android.graphics.Color;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -16,96 +14,94 @@ import edu.cnm.deepdive.codebreaker.R;
 import edu.cnm.deepdive.codebreaker.adapter.GuessAdapter;
 import edu.cnm.deepdive.codebreaker.model.Code.Guess;
 import edu.cnm.deepdive.codebreaker.model.Game;
+import edu.cnm.deepdive.codebreaker.viewmodel.MainViewModel;
 import java.security.SecureRandom;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity implements OnClickListener{
+public class MainActivity extends AppCompatActivity {
 
-    private static final String POOL = "ROYGBIV";
-    private static final int CODE_LENGTH = 4;
     private static final int[] colorValues =
-        {Color.RED, 0xffffa500, Color.YELLOW, Color.GREEN, Color.BLUE, 0xff4b0082, 0xffee82ee};
-    private static final Map<Character, Integer> colorMap =
-        buildColorMap(POOL.toCharArray(), colorValues);
+      {Color.RED, 0xffffa500, Color.YELLOW, Color.GREEN, Color.BLUE, 0xff4b0082, 0xffee82ee};
+  private static final Map<Character, Integer> colorMap =
+      buildColorMap(MainViewModel.POOL.toCharArray(), colorValues);
 
-    private ListView guessList;
-    private EditText guess;
-    private Button submit;
-    private Game game;
-    private GuessAdapter adapter;
-    private SecureRandom rng;
+  private ListView guessList;
+  private EditText guess;
+  private Button submit;
+  private Game game;
+  private GuessAdapter adapter;
+  private SecureRandom rng;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        guessList = findViewById(R.id.guess_list);
-        guess = findViewById(R.id.guess);
-        submit = findViewById(R.id.submit);
-        submit.setOnClickListener(this);
-        adapter = new GuessAdapter(this, colorMap);
-        rng = new SecureRandom();
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    guessList = findViewById(R.id.guess_list);
+    guess = findViewById(R.id.guess);
+    submit = findViewById(R.id.submit);
+    submit.setOnClickListener((view) -> recordGuess());
+    adapter = new GuessAdapter(this, colorMap);
+    rng = new SecureRandom();
+    startGame();
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    super.onCreateOptionsMenu(menu);
+    getMenuInflater().inflate(R.menu.options, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    boolean handled = true;
+    switch (item.getItemId()) {
+      case R.id.new_game:
         startGame();
+        break;
+      case R.id.restart_game:
+        restartGame();
+        break;
+      default:
+        handled = super.onOptionsItemSelected(item);
     }
+    return handled;
+  }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.options, menu);
-        return true;
+  private void recordGuess() {
+    try {
+      String text = this.guess.getText().toString().toUpperCase();
+      Guess guess = game.guess(text);
+      adapter.add(guess);
+      guessList.setSelection(adapter.getCount() - 1);
+      this.guess.setText("");
+    } catch (IllegalArgumentException e) {
+      Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
     }
+  }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        boolean handled = true;
-        switch (item.getItemId()) {
-            case R.id.new_game:
-                startGame();
-                break;
-            case R.id.restart_game:
-                restartGame();
-                break;
-            default:
-                handled = super.onOptionsItemSelected(item);
-        }
-        return handled;
-    }
+  private void startGame() {
+    game = new Game(MainViewModel.POOL, MainViewModel.CODE_LENGTH, rng);
+    resetList();
+  }
 
-    @Override
-    public void onClick(View v) {
-        try {
-            String text = this.guess.getText().toString().toUpperCase();
-            Guess guess = game.guess(text);
-            adapter.add(guess);
-            guessList.setSelection(adapter.getCount() - 1);
-            this.guess.setText("");
-        } catch (IllegalArgumentException e) {
-            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
-        }
-    }
+  private void restartGame() {
+    game.restart();
+    resetList();
+  }
 
-    private void startGame() {
-        game = new Game(POOL, CODE_LENGTH, rng);
-        resetList();
-    }
+  private void resetList() {
+    adapter.clear();
+    guessList.setAdapter(adapter);
+  }
 
-    private void restartGame() {
-        game.restart();
-        resetList();
+  private static Map<Character, Integer> buildColorMap(char[] chars, int[] values) {
+    Map<Character, Integer> colorMap = new HashMap<>();
+    for (int i = 0; i < chars.length; i++) {
+      colorMap.put(chars[i], values[i]);
     }
-
-    private void resetList() {
-        adapter.clear();
-        guessList.setAdapter(adapter);
-    }
-
-    private static Map<Character, Integer> buildColorMap(char[] chars, int[] values) {
-        Map<Character, Integer> colorMap = new HashMap<>();
-        for (int i = 0; i < chars.length; i++) {
-            colorMap.put(chars[i], values[i]);
-        }
-        return colorMap;
-    }
+    return colorMap;
+  }
 
 }
